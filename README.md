3218773019007117084b5c2bdb661105472553de
Rickard Hallerbäck <rickard.hallerback@gmail.com>
2023-04-23 11:51:44 +0200

    image publisher and subscriber

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Running the foxy docker port
 
 ```
-docker run -it -v $(pwd):/root/smarter osrf/ros:foxy-desktop 
+docker network create ros2_network
+docker run -it -v $(pwd):/root/smarter --network ros2_network osrf/ros:foxy-desktop 
 cd ~/smarter
 # Go to a project to build
 cd smarter_hello_world
@@ -30,5 +31,23 @@ Build and run:
 colcon build --symlink-install
 ros2 run my_package smarter_test_hello_world
 ```
+
+# Simple topic publisher and subscriber in the command line
+
+````bash
+ros2 topic echo /magical_number std_msgs/msg/Int8
+ros2 topic pub --once /magical_number std_msgs/msg/Int8 "data: 42"
+```
+
+## Issues with ROS in Docker (shared memory feature/bug in fast-dds)
+
+Test ros:rolling instead of ros:foxy. This seems to be a major obstacle in
+the current fast-dds implementation. It relies on shared memory by default.
+
+```
+docker build -t ros2_foxy_opensplice:custom -f Dockerfile.opensplice .
+docker run -it --rm -e RMW_IMPLEMENTATION=rmw_opensplice_cpp --network ros2_network ros2_foxy_opensplice:custom
+```
+
 
 
---
+++ smarter_image_processing/src/image_publisher_node.cpp
@@ -0,0 +1,52 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "cv_bridge/cv_bridge.h"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+
+class ImagePublisher : public rclcpp::Node
+{
+public:
+  ImagePublisher(const std::string& folder_path)
+  : Node("image_publisher_node")
+  {
+    pub_ = this->create_publisher<sensor_msgs::msg::Image>("/usb_cam/image_raw", 10);
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&ImagePublisher::timerCallback, this));
+    folder_path_ = folder_path;
+  }
+
+private:
+  void timerCallback()
+  {
+    cv::Mat img = cv::imread(folder_path_ + "/" + std::to_string(count_) + ".jpg");
+    if (img.empty()) {
+      std::cout << "End of images" << std::endl;
+      rclcpp::shutdown();
+      return;
+    }
+    count_++;
+
+    sensor_msgs::msg::Image::SharedPtr msg_out = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", img).toImageMsg();
+    pub_->publish(*msg_out);
+  }
+
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::string folder_path_;
+  int count_ = 0;
+};
+
+int main(int argc, char **argv)
+{
+  if (argc < 2) {
+    std::cout << "Please provide folder path as input argument" << std::endl;
+    return 0;
+  }
+
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ImagePublisher>(argv[1]));
+  rclcpp::shutdown();
+  return 0;
+}
